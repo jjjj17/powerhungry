e3e0f6491bf2478e891a1a7b60e030fcc4c971cd
javierjavier <jrojasr94@live.cl>
2020-08-09 14:51:25 -0400

    delete 0 & add product

--- powehungry.py
+++ powehungry.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-array = [2,0,2,2,0,2,0,2,2,0,2,0,2,2,0,2,0,2,2,0,2,0,2,2,0,2,0,2,2,0,2,0,2,2,0,2,0,2,2,0,2,0,2,2,0,2,0,2]
+array = [-2,-3,4,-5]
 
 def solution(xs):
+    #limpieza
     matrix = np.array(xs)
+    new_matrix = np.delete(matrix, np.where(matrix == 0)) #delete zeroes to keep product from being 0
+    product = np.prod(new_matrix)
+    
     negatives = len([x for x in xs if x<0])
     positives = len([x for x in xs if x>0])
-    zeros = len([x for x in xs if x == 0])
-    #return negatives, positives, zeros
-    return(matrix)
+    
+    return(f"{product}")
+    
 
 
 
-a,b,c = solution(array)
-
-print(f"numero de negativos: {a}")
-print(f"numero de positivos: {b}")
-print(f"numero de ceros: {c}")
+print(solution(array))
