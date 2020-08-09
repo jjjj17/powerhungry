119699f2616638f9a3ef46744eebbaa04ede80df
javierjavier <jrojasr94@live.cl>
2020-08-09 18:55:26 -0400

    cleaner code

--- powehungry.py
+++ powehungry.py
@@ -1,25 +1,16 @@
 import numpy as np
 
-array = [4,-5,0,38,-9]
+array = [-5,0,38,-9]
 
 def solution(xs):
-    #limpieza
     matrix = np.array(xs)
     new_matrix = np.sort(np.delete(matrix, np.where(matrix == 0))) #delete zeroes to keep product from being 0)
-
     negatives = [x for x in xs if x<0]
-
-    print(xs)
-
     if len(negatives)%2 != 0:
-        print("not hey")
-        min_neg = min(map(abs, negatives))
-        neg_delete = min_neg*-1
-
+        neg_delete = (min(map(abs, negatives)))*-1
         final_matrix = np.delete(new_matrix, np.where(new_matrix == neg_delete))
         return(f"{np.prod(final_matrix)}")
     else:
-        print("hey")
         return(f"{np.prod(new_matrix)}")
 
     
