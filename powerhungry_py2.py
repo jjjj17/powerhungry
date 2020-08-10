874ba071ae6e59be134bb8c44cd7846927328d4c
javierjavier <jrojasr94@live.cl>
2020-08-10 01:01:23 -0400

    output to str

--- powerhungry_py2.py
+++ powerhungry_py2.py
@@ -1,8 +1,7 @@
-import numpy as np
-
 array = [2,3,-2,-4,0,5,-9,-5,-2,32,32,1001,0,0,0,0,0,0,-4,-4,-4,-74,-4,-4,-4,-4,-4,-4,4,-2,-2]
 
 def solution(xs):
+    prod = 1
     if 0 < len(xs) < 50:
         arr_list = sorted([x for x in xs if x != 0 or abs(x) > 1000])
         print(arr_list)
@@ -14,7 +13,6 @@ def solution(xs):
             print(neg_delete)
             amount = arr_list.count(neg_delete)
             print(amount)
-            prod = 1
             if amount > 1:
                 arr_list.remove(neg_delete)
                 rep += (amount-1) * [neg_delete]
@@ -23,17 +21,18 @@ def solution(xs):
                 print(final_list)
                 for x in arr_list:
                     prod = prod * x
-                return(prod)
+                return("%s"%prod)
             else:
                 pass
                 arr_list.remove(neg_delete)
                 final_list = arr_list + rep
                 for x in arr_list:
                     prod = prod * x
-                return(prod)
+                return("%s"%prod)
         else:
             for x in arr_list:
                 prod = prod * x
+            return("%s"%prod)
 
 
 print(solution(array))
