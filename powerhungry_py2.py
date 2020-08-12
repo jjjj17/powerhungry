30d38e969d7747622bd9db08054ace735d2fc5f3
javierjavier <javier.rojas1@walmart.com>
2020-08-11 21:21:26 -0400

    cleaner code and fix return None exceptions

--- powerhungry_py2.py
+++ powerhungry_py2.py
@@ -1,38 +1,22 @@
-array = [2,3,-2,-4,0,5,-9,-5,-2,32,32,1001,0,0,0,0,0,0,-4,-4,-4,-74,-4,-4,-4,-4,-4,-4,4,-2,-2]
-
 def solution(xs):
-    prod = 1
     if 0 < len(xs) < 50:
-        arr_list = sorted([x for x in xs if x != 0 or abs(x) > 1000])
-        print(arr_list)
         negatives = [x for x in xs if x<0]
-        print(len(negatives))
-        rep = []
+        positives = [x for x in xs if x>0]
         if len(negatives)%2 != 0:
-            neg_delete = (min(map(abs, negatives)))*-1
-            print(neg_delete)
-            amount = arr_list.count(neg_delete)
-            print(amount)
-            if amount > 1:
-                arr_list.remove(neg_delete)
-                rep += (amount-1) * [neg_delete]
-                print(rep)
-                final_list = arr_list + rep
-                print(final_list)
-                for x in arr_list:
-                    prod = prod * x
-                return("%s"%prod)
-            else:
-                pass
-                arr_list.remove(neg_delete)
-                final_list = arr_list + rep
-                for x in arr_list:
-                    prod = prod * x
-                return("%s"%prod)
-        else:
-            for x in arr_list:
-                prod = prod * x
+            negatives.sort()
+            negatives.pop()
+        values = negatives + positives
+        if values:
+            prod = 1
+            for value in values:
+                print(value)
+                prod = prod * value
             return("%s"%prod)
+        else:
+            return("0")
+    else:
+        return("0")
 
 
+array = [-4,-2,-4]
 print(solution(array))
