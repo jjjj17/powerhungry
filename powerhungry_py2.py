7a6d580b63192152063bc55cc2fe1396a999c04d
javierjavier <jrojasr94@live.cl>
2020-08-11 23:42:44 -0400

    final solution

--- powerhungry_py2.py
+++ powerhungry_py2.py
@@ -1,15 +1,14 @@
 def solution(xs):
-    if 0 < len(xs) < 50:
-        negatives = [x for x in xs if x<0]
-        positives = [x for x in xs if x>0]
-        if len(negatives)%2 != 0:
-            negatives.sort()
-            negatives.pop()
+    if 0 < len(xs) < 51:
+        negatives = [x for x in xs if -1001<x<0]
+        positives = [x for x in xs if 1001>x>0]
+        if (len(negatives)%2 != 0 and len(xs)>1):
+          negatives.sort()
+          negatives.pop()
         values = negatives + positives
         if values:
-            prod = 1
+            prod = long(1)
             for value in values:
-                print(value)
                 prod = prod * value
             return("%s"%prod)
         else:
@@ -18,5 +17,7 @@ def solution(xs):
         return("0")
 
 
-array = [-4,-2,-4]
-print(solution(array))
+arrays = [[-4,0], [-4]]
+
+for x in arrays:
+    print(solution(x))
